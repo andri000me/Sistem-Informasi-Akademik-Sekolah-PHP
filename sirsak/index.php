a47f08f62025bb2799edb84e581706e8ebeb3f9d
Nikko Edy Raharja <39364571+nikoedyr@users.noreply.github.com>
2021-02-10 13:56:58 +0700

    Update index.php

--- sirsak/index.php
+++ sirsak/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Sistem Informasi Akademik Sekolah</title>
+<title>Sistem Informasi Akademik Sekolah PHP </title>
 </head>
 <body background="bg.png">
 <div>
